f5c45f8a5f0bdd0a1081933e3cbc30ef0f681590
Ramunas <rambek@gmail.com>
2020-02-28 12:24:04 +0200

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,44 @@
+let myIndex = 0;
+
+function carousel() {
+    let i;
+    let x = document.getElementsByClassName("hero");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1;
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 6000);
+}
+
+carousel();
+
+let slideIndex = 1;
+
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("hero");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+showSlides(slideIndex);
